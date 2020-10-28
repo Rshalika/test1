fd56897f250f24807b77953e28f1d970d73d1f01
GitHub Action <action@github.com>
2020-10-28 12:32:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/rshalika/test1/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rshalika/test1/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rshalika.test1.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rshalika.test1.MyBundle
 
 class MyApplicationService {
 
